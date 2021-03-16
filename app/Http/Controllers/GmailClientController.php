591bf9cceb11c3a3fe34f2a4b9847083650da698
정주호 <meju7015@gmail.com>
2021-03-16 16:46:51 +0900

    Fix 컨트롤러 삭제
    .

--- app/Http/Controllers/GmailClientController.php
+++ app/Http/Controllers/GmailClientController.php
@@ -12,57 +12,5 @@ use Illuminate\Support\Str;
 
 class GmailClientController extends Controller
 {
-    function getGmailClient()
-    {
 
-        $client = new GoogleClient('me', '6ohc7reoj9nbp5jccs2ldkknl4@group.calendar.google.com');
-
-        $prev = $client->getCacheInbox();
-        $origin = $client->getThreads(5)->getThreads();
-        $next = $origin;
-
-        if ($prev === null) {
-            $client->setCacheInbox($next);
-            $prev = $client->getCacheInbox();
-        }
-
-        Log::info('******** 스케쥴링 시작 ********');
-
-        foreach ($prev as $pk => $pi) {
-            foreach ($next as $ni) {
-                if ($pi->id === $ni->id) {
-                    unset($prev[$pk]);
-                }
-            }
-        }
-
-        foreach ($prev as $key => $item) {
-            $parser = new GmailParser(
-                $client,
-                $item->id
-            );
-
-            $body = $parser->parseMessageBodyToFormat();
-
-            if (!empty($body['job'])) {
-                $client->createEvent([
-                    'summary' => $body['job'],
-                    'description' => $body['issue'],
-                    'start' => [
-                        'dateTime' => Carbon::make($body['work_date'][0])->format('Y-m-d\TH:i:s'),
-                        'timeZone' => 'America/Los_Angeles'
-                    ],
-                    'end' => [
-                        'dateTime' => Carbon::make($body['work_date'][1])->format('Y-m-d\TH:i:s'),
-                        'timeZone' => 'America/Los_Angeles'
-                    ]
-                ]);
-
-                Log::info(`{$body['job']} :: 캘린더에 추가됨. - `.Carbon::now()->format('Y-m-d H:i:s'));
-            }
-        }
-
-        $client->setCacheInbox($origin);
-        Log::info('******** 스케쥴링 끝 ********');
-    }
 }
